6aaee368a34b9cc1e14f8517d091c9f725156867
Patrick Proctor <patrick42h@gmail.com>
2013-03-12 18:51:24 -0400

    Made Google Custom Search search bar in left-hand column be specific about the URL of the search results page, rather than what is set in the GCSE control panel.

--- partials/left-sidebar.php
+++ partials/left-sidebar.php
@@ -2,7 +2,7 @@
 	<?php menus(); ?>
 	<?php if (!is_page('search')): ?>
 		<div class="searchbox">
-			<gcse:searchbox-only></gcse:searchbox-only>
+			<gcse:searchbox-only resultsUrl="<?php echo get_page_link(get_page_by_title('Search results','OBJECT')); ?>"></gcse:searchbox-only>
 		</div>
 	<?php endif; ?>
 </div>
